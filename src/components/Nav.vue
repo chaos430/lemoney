7852703bad33a55f377bfaea4ca20832d47d8b12
chaos430 <1192318530@qq.com>
2021-05-29 10:41:59 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link class="item" to="/money">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">
+
+    <router-link class="item" to="/labels">
       <Icon name="label"/>
       标签</router-link>
-    |
-    <router-link to="/statistics">
+
+    <router-link class="item" to="/statistics">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -26,5 +26,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+nav{
+  display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+        .icon{
+          width: 32px;
+          height: 32px;
+        }
+  }
+}
 
 </style>
