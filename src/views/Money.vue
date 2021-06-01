ea4a143f1054e11b2b09b9c89fa3aecd7bb99bb0
chaos430 <1192318530@qq.com>
2021-06-01 14:56:46 +0800

    创建标签页

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,20 +16,23 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
-import {model} from '@/model';
-const recordList = model.fetch()
+import {recordListModel} from '@/models/recordListModel';
+import {tagListModel} from '@/models/tagListModel';
 
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 
 @Component({
   components: {Tags, Notes, Types, NumberPad},
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行'];
+  tags = tagList;
   // eslint-disable-next-line no-undef
-  recordList: RecordItem[] = recordList
+  recordList: RecordItem[] = recordList;
   // eslint-disable-next-line no-undef
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
@@ -37,17 +40,19 @@ export default class Money extends Vue {
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
-  saveRecord(){
+
+  saveRecord() {
 
     // eslint-disable-next-line no-undef
-    const record2: RecordItem = model.clone(this.record)
-    record2.createdAt = new Date()
-    this.recordList.push(record2)
+    const record2: RecordItem = recordListModel.clone(this.record);
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
   }
-@Watch('recordList')
-  onRecordListChange(){
-  model.save(this.recordList)
-}
+
+  @Watch('recordList')
+  onRecordListChange() {
+    recordListModel.save(this.recordList);
+  }
 }
 </script>
 <style lang="scss">
