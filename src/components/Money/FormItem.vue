9df47fd43650287df0acd6be5fcc3f4f708c52b6
chaos430 <1192318530@qq.com>
2021-06-01 19:54:13 +0800

    EditLabel.vue样式添加完成 抽离了两个组件 Button 和FormItem

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,30 +1,30 @@
 <template>
 
   <div>
-    <label class="notes"><span class="name">备注</span> <input type="text" v-model="value" placeholder="在这里输入备注">
+    <label class="formItem"><span class="name">{{this.fieldName}}</span> <input type="text" v-model="value" :placeholder="this.placeholder">
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component,Watch} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
-export default class Notes extends Vue {
+export default class FormItem extends Vue {
   value = '';
+  @Prop({required:true}) fieldName!:string
+  @Prop() placeholder?: string
   @Watch('value')
   onValueChanged(value:string){
     this.$emit('update:value',value)
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
-.notes {
+.formItem {
   font-size: 14px;
-  background: #f5f5f5;
   display: flex;
   align-items: center;
 
@@ -33,7 +33,7 @@ export default class Notes extends Vue {
   }
 
   input {
-    height: 64px;
+    height: 40px;
     flex-grow: 1;
     background: transparent;
     border: none;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,10 @@
     <Layout class-prefix="layout">
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
-      <Notes @update:value="onUpdateNotes"/>
+      <div class="notes">
+        <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+      </div>
+
       <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
     </Layout>
   </div>
@@ -13,7 +16,7 @@
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
+import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 import {recordListModel} from '@/models/recordListModel';
@@ -24,7 +27,7 @@ const tagList = tagListModel.fetch();
 
 
 @Component({
-  components: {Tags, Notes, Types, NumberPad},
+  components: {FormItem, Tags, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = tagList;
@@ -59,5 +62,8 @@ export default class Money extends Vue {
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
+}
+.notes{
+  padding: 12px 0;
 }
 </style>
