111355b761f07e81f456fcfe116937de528f4e24
chaos430 <1192318530@qq.com>
2021-06-05 12:06:53 +0800

    将所有数据操作封装在 store里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,6 +20,7 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 
 
@@ -27,8 +28,8 @@ import {Component} from 'vue-property-decorator';
   components: {FormItem, Tags, Types, NumberPad},
 })
 export default class Money extends Vue {
-  tags = window.tagList;
-  recordList = window.recordList;
+  tags = store.tagList;
+  recordList = store.recordList;
   // eslint-disable-next-line no-undef
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
@@ -41,7 +42,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    window.createRecord(this.record)
+    store.createRecord(this.record)
   }
 
 }
