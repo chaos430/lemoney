858195ce74cd6c7a6a3d578186a191bacce15cc7
chaos430 <1192318530@qq.com>
2021-06-05 17:04:52 +0800

    Tags.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -21,13 +21,17 @@ import {Component} from 'vue-property-decorator';
 
 @Component({
   computed: {
-    tagList(){
-      return []
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -41,8 +45,8 @@ export default class Tags extends Vue {
 
   create() {
     const name = window.prompt('请输入标签名');
-    if (!name) { return  window.alert('标签名不能为空');}
-    //store.createTag(name);
+    if (!name) { return window.alert('标签名不能为空');}
+    this.$store.commit('createTag', name);
   }
 }
 
