2a44009ab86ab634a140c0606c300b81faaafc43
chaos430 <1192318530@qq.com>
2021-06-05 22:34:55 +0800

    修复刷新变成404bug

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -29,29 +29,23 @@ export default class EditLabel extends Vue {
   }
   created() {
     const id = this.$route.params.id
+    this.$store.commit('fetchTags')
     this.$store.commit('setCurrentTag',id)
     if (!this.tag) {
       this.$router.replace('/404');
     }
   }
 
-  update() {
+  update(name:string) {
     if (this.tag) {
-      //todo
-      //store.updateTag(this.tag.id, name);
+      this.$store.commit('updateTag',{id:this.tag.id,name})
     }
   }
 
   remove() {
     if (this.tag) {
-      //
-      return
-  //     if (store.removeTag(this.tag.id)) {
-  //       this.$router.back();
-  //     } else {
-  //       store.alert('删除失败');
-  //     }
-   }
+      this.$store.commit('removeTag',this.tag.id)
+    }
    }
 
   goBack() {
